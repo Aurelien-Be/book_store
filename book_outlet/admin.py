946310cbae9885892a1b389a758bec0ce5507cd2
Aurelien-Be <abeveraggi@netcourrier.com>
2021-10-17 12:04:16 +0300

    my changes

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Author, Book, Adress, Country
+
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    #then we can have some fields only readable 
+    #readonly_fields = ("slug") but we don't because we want a preview
+    prepopulated_fields = {"slug" : ("title",)}
+    list_filter = ("author", "rating")
+    #to have columns 
+    list_display = ("title","author", "rating")
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Adress)
+admin.site.register(Country)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,26 +1,70 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models.deletion import CASCADE
 from django.urls import reverse 
 from django.utils.text import slugify #transform a string to a slug
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=3)
+    def __str__(self):
+        return f"{self.name}"
+    
+    #to set how the Adress module is outputed
+    class Meta:
+        verbose_name_plural = "Countries"
+
+class Adress(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+     
+
+    def __str__(self):
+        return f"{self.street} {self.postal_code} {self.city}"
+    
+    #to set how the Adress module is outputed
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    adress = models.OneToOneField(Adress, on_delete=CASCADE, null=True)
+   
+
+#to print the class as a string when needed :
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    def __str__(self): 
+        return self.full_name() 
+    
+     
 
 #we inherate a built in class in django 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
-    is_bestselling = models.NullBooleanField(default=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books") #we point the preview class Author to make the relationship    # cascade = if author is deleted, then also the book in the data. 
+    is_bestselling = models.BooleanField(default=False)
+    #related_name allows to access to Author.books to query to all the books of an author
     #we can add a new slug attribute in order to have a nicer url than a simple number, for that django has a specific field
-    slug = models.SlugField(default="", null=False, db_index=True) #harry-potter-1
-    #add db_index improve the performance, as we use often this filter
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True) #harry-potter-1
+    #add db_index=True improve the performance, as we use often this filter
+    #add editable = False to make impossible to edit in admin. But we cant do it if you use the preview feature
+    #in order to make the slug field not mandatory in admin, we can add blank=True. Anyway, our function will edit the slug when we save
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
 
     #we can overwrite the save function, in order to set the value other than empty is set whenhever we save
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title) #the slug is added for all our models
-        super().save(*args, **kwargs) #we make sur that the supermethod is called 
+    #it is not useful with the prepopulated feature in admin, so I comment it
+    #def save(self, *args, **kwargs):
+    #    self.slug = slugify(self.title) #the slug is added for all our models
+    #    super().save(*args, **kwargs) #we make sur that the supermethod is called 
 
 
 
